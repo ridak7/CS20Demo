ebc64c1b7b6de56c94c8e6f4555297d1533f4eed
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2022-10-11 09:39:56 -0600

    9:39AM

--- Chapter6/src/Exp1.java
+++ Chapter6/src/Exp1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Exp1 
 {
@@ -19,15 +20,74 @@ public class Exp1
 	
 	public static String getLetterGrade(int numGrade)
 	{
+		if(numGrade < 60)
+			return("D");
 		
+		else if(numGrade < 70)
+		{
+			if(numGrade == 69)
+				return("C");
+			else
+				return("C-");
+		}			
+		
+		else if(numGrade < 80)
+		{
+			if(numGrade == 79)
+				return("B");
+			else
+				return("B-");
+		}
+		
+		else if(numGrade < 90)
+		{
+			if(numGrade == 89)
+				return("A");
+			else
+				return("A-");
+		}
+		
+		else if(numGrade < 100)
+				return("A+");
+		
+		else
+			return ("A+");
+			
 	}
-	
-	
 	
 	
 		
 	public static void main(String[] args) 
 	{
+		final int minValue = 0;
+		final int maxValue = 100;
+		int numericGrade;
+		String letterGrade;
+		
+		Scanner input = new Scanner(System.in);
+		
+		//Obtain user input 
+		System.out.print("Enter a numeric grade (-1 to quit): ");
+		numericGrade = input.nextInt();//get an integer from the user
+		
+		while(numericGrade != -1)
+		{
+			if(isValidNumber(numericGrade, minValue, maxValue))
+			{
+				//Calling function to return letter grade associated with numericGrade
+				letterGrade = getLetterGrade(numericGrade);
+				System.out.print("The grade " + numericGrade + " is a(n) "+ letterGrade + ".");
+			}
+			else
+			{
+				System.out.print("Grade entered is not valid. ");
+			}
+			
+			//Obtain user input again before leaving the loop
+			System.out.print("Enter a numeric grade (-1 to quit): ");
+			numericGrade = input.nextInt();//get an integer from the user
+		}
+		
 		
 
 	}
