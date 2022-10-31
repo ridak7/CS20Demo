b97c10010bac98cbeee8525da3fc46fe8960da5d
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2022-10-31 11:31:46 -0600

    11:31AM

--- Chapter2/src/MyInitials.java
+++ Chapter2/src/MyInitials.java
@@ -3,13 +3,7 @@ public class MyInitials {
 
 	public static void main(String[] args) 
 	{
-		System.out.print("\t| \t");
-		System.out.print("| \n");
-		//System.out.println("");
-		System.out.print("\t| ----- ");
-		System.out.print("| \n");
-		System.out.print("\t| \t");
-		System.out.print("| \n");
+		System.out.print(Math.random()*20 + 10);
 		
 
 	}
---
+++ Chapter7/src/TestCircle.java
@@ -0,0 +1,17 @@
+
+public class TestCircle 
+{
+
+	public static void main(String[] args) 
+	{
+		Circle c1 = new Circle();
+		
+		System.out.println(c1.getRadius());
+		
+		c1.setRadius(3.9);
+		
+		System.out.println(c1.getRadius());
+
+	}
+
+}
