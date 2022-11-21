27c3921a642bb4f0a3737171cfa547b462a8fdb3
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2022-11-21 13:10:47 -0700

    1:08PM

---
+++ Chapter9P3/src/Lesson1onArrays.java
@@ -0,0 +1,15 @@
+
+public class Lesson1onArrays 
+{
+
+	public static void main(String[] args) 
+	{
+		//How do you declare an Array
+		String[] studentNames;
+		
+		//How do I initialize an Array
+		String[] studentName = new String[5];
+
+	}
+
+}
