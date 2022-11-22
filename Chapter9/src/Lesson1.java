4bd81d051945f53f31eb1c53353f541784ef830d
amabdalla <amabdalla@E815WL170096.org.cbe.ab.ca>
2022-11-22 09:30:49 -0700

    9:30AM

--- Chapter9/src/Lesson1.java
+++ Chapter9/src/Lesson1.java
@@ -12,11 +12,26 @@ public class Lesson1 {
 		//Declare, allocate, and initialize at the same time
 		String[] studentNames = {"Li", "Ali", "Krista", "Taimoor", "Kevin"};
 		
-		System.out.println(studentNames[2]);
-		System.out.println(studentNames[4]);
+		//System.out.println(studentNames[2]);
+		//System.out.println(studentNames[4]);
 		
-		studentNames[2] = "Haydu";
-		System.out.println(studentNames[2]);
+		//studentNames[2] = "Haydu";
+		//System.out.println(studentNames[2]);
+		
+		//System.out.println(studentNames.length);
+		
+		/*for(int i=0; i < studentNames.length; i++)
+		{
+			System.out.println(studentNames[i]);
+		}
+		*/
+		
+		for(String element: studentNames)
+		{
+			System.out.println(element);
+		}
+		
+		
 
 	}
 
